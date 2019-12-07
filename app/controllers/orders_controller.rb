5f10e69c4db19f3670b4415a0f8011f9c4bfd993
kohei-itano <itanokohei33@gmail.com>
2019-12-07 12:21:56 +0000

    product_order_model_cont_view_set

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+
+# 顧客の注文履歴一覧ページ
+	def index
+	end
+
+# 注文履歴詳細ページ
+	def show
+	end
+
+# 顧客の購入情報の入力画面
+	def new
+	end
+
+# 情報の保存
+	def create
+	end
+
+# 注文完了画面
+	def finish
+	end
+
 end
+
