69060297fb0a2e5f426417c8c20903b5903fb51f
kennsuke906 <dashijianyou@gmail.com>
2019-12-07 08:58:30 +0000

    ship_to_adresses_model_finish

--- db/migrate/20191207072823_create_ship_to_adresses.rb
+++ db/migrate/20191207072823_create_ship_to_adresses.rb
@@ -1,6 +1,9 @@
 class CreateShipToAdresses < ActiveRecord::Migration[5.2]
   def change
     create_table :ship_to_adresses do |t|
+      t.string :name,         null: false
+      t.string :postal_code,  null: false
+      t.text :address,        null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_042319) do
+ActiveRecord::Schema.define(version: 2019_12_07_072823) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,12 +24,27 @@ ActiveRecord::Schema.define(version: 2019_12_07_042319) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "ship_to_adresses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.text "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postal_code", null: false
+    t.text "address", null: false
+    t.string "phone", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
