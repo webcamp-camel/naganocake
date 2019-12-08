d524ce14a5deb75eed60b8190051fd001724ba4d
SShinjo <skskosmn@gmail.com>
2019-12-08 08:35:36 +0000

    cart_items controller edit

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,7 +1,11 @@
 class CartItemsController < ApplicationController
   def index
+  	@user = current_user
+  	@cart_items = @user.cart_items
   end
 
   def show
+  	@product = Product.find(params[:product_id])
+  	@cart_item = @product.cart_items
   end
 end
