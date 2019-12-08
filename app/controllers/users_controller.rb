a440be18f86e584ea594341608c4a2bae42f5032
SShinjo <skskosmn@gmail.com>
2019-12-08 08:01:55 +0000

    edit user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
+
+before_action :set_user
+
+
   def show
-  	@user = current_user
   end
 
   def edit
-  	@user = current_user
   end
 
   def update
+  	@user.update(user_params)
   end
+
+  private
+  	def set_user
+  		@user = current_user
+  	end
+
+  	def user_params
+  		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :phone, :email)
+  	end
 end
