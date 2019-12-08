cf43ae86ce38015c641ea6cb333f74f2ec1fe649
SShinjo <skskosmn@gmail.com>
2019-12-08 08:52:26 +0000

    cart_items controller modified

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,33 @@
 class CartItemsController < ApplicationController
+
+  before_action: set_user, only: [:add_items, :index, :all_destroy]
+  before_action: set_product, only: [:show, :destroy]
+
+  def add_items
+  end
+
   def index
-  	@user = current_user
-  	@cart_items = @user.cart_items
   end
 
   def show
-  	@product = Product.find(params[:product_id])
-  	@cart_item = @product.cart_items
   end
+
+  def destroy
+  	@cart_item.destroy
+  end
+
+  def all_destroy
+  	@cart_items.destroy
+  end
+
+  private
+  	def set_user
+  		@user = current_user
+  		@cart_items = @user.cart_items
+  	end
+
+  	def set_product
+  		@product = Product.find(params[:product_id])
+  		@cart_item = @product.cart_item
+  	end
 end
