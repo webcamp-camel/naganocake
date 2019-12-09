a3a9f6f077adcedd89bcb22da7cf985cb90c35d0
SShinjo <skskosmn@gmail.com>
2019-12-09 08:14:10 +0000

    modified admins/genres

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,8 +1,16 @@
 class Admins::GenresController < ApplicationController
+  #ジャンル管理画面
   def index
+  	@genre = Genre.new
   end
 
   def create
+  	@genre = Genre.new(genre_params)
+  	if @genre.save
+  		redirect_to genres_path
+  	else
+  		render :index
+  	end
   end
 
   def edit
@@ -10,4 +18,9 @@ class Admins::GenresController < ApplicationController
 
   def update
   end
+
+  private
+  	def genre_params
+  		params.require(:genre).permit(:name,:is_disable)
+  	end
 end
