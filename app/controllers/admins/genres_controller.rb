1e51d79ee6dc0c9956b499056c918263af6a949d
SShinjo <skskosmn@gmail.com>
2019-12-09 08:35:49 +0000

    genre create ok

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -2,12 +2,13 @@ class Admins::GenresController < ApplicationController
   #ジャンル管理画面
   def index
   	@genre = Genre.new
+  	@genres = Genre.all
   end
 
   def create
   	@genre = Genre.new(genre_params)
   	if @genre.save
-  		redirect_to genres_path
+  		redirect_to admins_genres_path
   	else
   		render :index
   	end
