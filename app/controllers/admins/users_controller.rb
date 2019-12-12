d5a1008b2c71dfda72f9fc14533286194a001261
SShinjo <skskosmn@gmail.com>
2019-12-12 05:32:44 +0000

    admins/user and routing set

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -18,8 +18,22 @@ class Admins::UsersController < ApplicationController
   	redirect_to admins_user_path(@user)
   end
 
+  def toggle
+  	@user = User.find(params[:id])
+
+  	if @user.is_deleted == false
+  		@user.is_deleted = true
+  		@user.save
+  		redirect_to edit_admins_user_path(@user)
+  	else
+  		@user.is_deleted = false
+  		@user.save
+  		redirect_to edit_admins_user_path(@user)
+  	end
+  end
+
   private
   	def user_params
-  		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :phone, :email)
+  		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :phone, :email, :is_deleted)
   	end
 end
