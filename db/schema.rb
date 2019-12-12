080817ea7a34a4eef3d45c90206510cac1c17502
kennsuke906 <dashijianyou@gmail.com>
2019-12-12 05:43:33 +0000

    wit

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,13 @@ ActiveRecord::Schema.define(version: 2019_12_11_143140) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.integer "ship_status", default: 1
+    t.integer "ship_status", null: false
     t.integer "payment", null: false
-    t.integer "total_price"
+    t.integer "total_price", null: false
     t.integer "postage", default: 800, null: false
     t.text "ship_address", null: false
-    t.string "ship_postal_code"
-    t.integer "deposit_status", default: 1
+    t.string "ship_postal_code", null: false
+    t.integer "deposit_status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "last_name"
