c428db4d44a3648dd5342176d66e26f43e35c400
yu-198 <fevrier39seize@gmail.com>
2019-12-15 03:15:48 +0000

    layouts update

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -2,8 +2,8 @@ class Admins::HomeController < ApplicationController
   def top
   	# @top = Order.find(params[:id])
   	# @top_comment = Order.new
-  	 @order = Order.find(params[:id])
-  	 @order_comment = Order.new
+  	 @count = Order.where(ship_status:1).count
+  	 @orders = Order.where(ship_status:1)
 
   end
 end
