2a818e12d7a3aeac07c9818e416f53cc31b2571c
SShinjo <skskosmn@gmail.com>
2019-12-17 08:36:02 +0000

    orders/controller

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -16,21 +16,24 @@ class Admins::OrdersController < ApplicationController
   def update
 #orderのdeposit_statusの更新
   	@order = Order.find(params[:id])
-    @order.deposit_status = params[:order][:deposit_status]
-    @order.save
-#ordered_itemのproduct_statusの更新
-    @items = @order.ordered_items
-      @items.each do |item|
-        item.product_status = params[:ordered_items][:product_status][item.id.to_s]
-        item.save
-      end
+    @order.update(order_params)
 
+#    @order.deposit_status = params[:order][:deposit_status]
+#    @order.save
+#ordered_itemのproduct_statusの更新
+#    @items = @order.ordered_items
+#      @items.each do |item|
+#        item.product_status = params[:ordered_items][:product_status][item.id.to_s]
+#        item.save
+#      end
+
+     pp @order.errors.full_messages
   	redirect_to admins_orders_path
   end
 
   private
   def order_params
-  	params.require(:order).permit(:product_status, :deposit_status, :product_status)
+  	params.require(:order).permit(:deposit_status,ordered_items_attributes:[:id, :product_status])
   end
 
 end
