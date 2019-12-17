dc446a4fadb05ea1d756aa1a613e3a677fbec315
SShinjo <skskosmn@gmail.com>
2019-12-17 07:42:53 +0000

    order/controller edit

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -12,14 +12,15 @@ class Admins::OrdersController < ApplicationController
   def update
 #orderのdeposit_statusの更新
   	@order = Order.find(params[:id])
-    @order.deposit_status = params[:order][:deposit_status].to_i
+    @order.deposit_status = params[:order][:deposit_status]
     @order.save
 #ordered_itemのproduct_statusの更新
     @items = @order.ordered_items
       @items.each do |item|
-        item.product_status = params[:ordered_items][:product_status][item.id.to_s].to_i
+        item.product_status = params[:ordered_items][:product_status][item.id.to_s]
         item.save
       end
+
   	redirect_to admins_orders_path
   end
 
